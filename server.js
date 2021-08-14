8e9ecd99839f395dd2120ca4c25d6d2c85d65462
choonchy <57583113+choonchy@users.noreply.github.com>
2021-08-15 04:51:35 +1000

    express session added

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 const routes = require('./controllers');
+const hbs = exphbs.create({ helpers });
 
+const sess = {
+	secret: 'this is a secret',
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -17,6 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
-app.listen(PORT, () => {
-	console.log('Server is active on address http://localhost:' + PORT);
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log('Now listening'));
 });
