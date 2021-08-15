6ad49c4789a99e5f9dcf06aace1b236b9716052c
choonchy <57583113+choonchy@users.noreply.github.com>
2021-08-15 20:48:59 +1000

    login and signup pages added

--- controllers/blog-routes.js
+++ controllers/blog-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
 	try {
@@ -17,6 +18,24 @@ router.get('/', async (req, res) => {
 		console.log(err);
 		res.status(500).json(err);
 	}
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {});
+
+router.get('/login', (req, res) => {
+	if (req.session.logged_in) {
+		res.redirect('/');
+		return;
+	}
+	res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+	if (req.session.logged_in) {
+		res.redirect('/');
+		return;
+	}
+	res.render('signup');
 });
 
 module.exports = router;
