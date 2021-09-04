8fcb30d28cd09bccf862c53fe38792a72e7672f6
choonchy <57583113+choonchy@users.noreply.github.com>
2021-09-04 14:33:42 +1000

    connection change

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: process.env.JAWSDB_URL,
-		dialect: 'mysql',
-		port: 3306,
-	}
-);
+const sequelize = new Sequelize(process.env.JAWSDB_URL, {
+	dialect: 'mysql',
+	port: 3306,
+});
 
 module.exports = sequelize;
