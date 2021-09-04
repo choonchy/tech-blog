4ea432e29c5bd75174c26d563cf7b053f71d231c
choonchy <57583113+choonchy@users.noreply.github.com>
2021-09-04 14:17:56 +1000

    jawsdb connection added

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
 	process.env.DB_USER,
 	process.env.DB_PASSWORD,
 	{
-		host: 'localhost',
+		host: 's29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 		dialect: 'mysql',
 		port: 3306,
 	}
