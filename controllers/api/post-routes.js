d5468e4e041d82b0bb2b2b8a742c6a6ba8436893
choonchy <57583113+choonchy@users.noreply.github.com>
2021-09-04 14:00:33 +1000

    final changes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User } = require('../../models');
+const { post } = require('../blog-routes');
 
 router.post('/new-post', async (req, res) => {
 	try {
@@ -11,6 +12,28 @@ router.post('/new-post', async (req, res) => {
 
 		console.log(postData);
 		res.json({ post: postData, message: 'Posted successfully!' });
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	try {
+		const postData = await Post.findOne({
+			include: User,
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		const post = postData.get({ plain: true });
+
+		console.log(postData);
+
+		res.render('post', {
+			post,
+			logged_in: req.session.logged_in,
+		});
 	} catch (err) {
 		res.status(400).json(err);
 	}
